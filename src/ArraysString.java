41f25e7d437b3eabfa0eb07c0aa3963bf60cf02a
DariuszSoltysiak <oneweb@wp.pl>
2023-04-12 23:01:34 +0200

    Initial commit

---
+++ src/ArraysString.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ArraysString {
+    public static void main(String[] args) {
+        boolean seek = false;
+        String[] familySoltysiak = {"Bogusia", "Darek", "Jacek", "Karolina", "Julek", "Sylwek", "Teresa"};
+
+        System.out.println("Give me the name");
+
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+
+        for (int i = 0; i < familySoltysiak.length; i++) {
+            if (familySoltysiak[i].equals(name)) {
+                seek = true;
+                break;
+            }
+        }
+        if (seek) {
+            System.out.println(name + " is a member of Soltysiak family");
+        } else {
+            System.out.println(name + " is not a member of Soltysiak family ");
+        }
+    }
+}
